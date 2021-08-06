d302b6400c6fe620515d17a6133a2c0a696a1e03
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-08-05 23:28:49 -0400

    added navbar and set up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+      </Switch>
     </div>
   );
 }
