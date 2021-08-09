34cf655f161b6f9cdeaaac190c9443077e352d18
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-08-09 17:12:08 -0400

    removed dummy post list

--- client/src/components/post/Feed.js
+++ client/src/components/post/Feed.js
@@ -2,37 +2,6 @@ import React, { useContext } from 'react';
 import PostContext from '../../context/post/PostContext';
 import Post from './Post';
 
-const postList = [
-    {
-        id: Math.random().toString(),
-        title: "This is the first post",
-        content: "Welcome to the very first post of this app",
-        likes: 3,
-        dislikes: 1
-    },
-    {
-        id: Math.random().toString(),
-        title: "This is the first post",
-        content: "Welcome to the very first post of this app",
-        likes: 3,
-        dislikes: 1
-    },
-    {
-        id: Math.random().toString(),
-        title: "This is the first post",
-        content: "Welcome to the very first post of this app",
-        likes: 3,
-        dislikes: 1
-    },
-    {
-        id: Math.random().toString(),
-        title: "This is the first post",
-        content: "Welcome to the very first post of this app",
-        likes: 3,
-        dislikes: 1
-    },
-];
-
 const Feed = () => {
     const postContext = useContext(PostContext);
 
