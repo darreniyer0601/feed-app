6d810edd0dec715792698425eebb3405ad02a0cb
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-09-03 20:46:13 -0400

    removed placeholder posts and comments

--- client/src/components/post/Feed.js
+++ client/src/components/post/Feed.js
@@ -2,43 +2,10 @@ import React, { useContext } from 'react';
 import PostContext from '../../context/post/PostContext';
 import Post from './Post';
 
-const posts = [
-    {
-        id: Math.random().toString(),
-        createdAt: Date.now,
-        edited: false,
-        likes: 1,
-        comments: 2,
-        title: 'The first post',
-        content: 'This is a new post',
-        displayName: 'Darren Iyer'
-    },
-    {
-        id: Math.random().toString(),
-        createdAt: Date.now,
-        edited: false,
-        likes: 1,
-        comments: 0,
-        title: 'The second post',
-        content: 'This is another post',
-        displayName: 'Darren Iyer'
-    },
-    {
-        id: Math.random().toString(),
-        createdAt: Date.now,
-        edited: false,
-        likes: 2,
-        comments: 1,
-        title: 'The newest post',
-        content: 'This is a great post',
-        displayName: 'Sam Smith'
-    },
-]
-
 const Feed = () => {
-    // const postContext = useContext(PostContext);
+    const postContext = useContext(PostContext);
 
-    // const { posts } = postContext;
+    const { posts } = postContext;
 
     return (
         <div className="card-deck">
